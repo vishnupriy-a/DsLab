193161544c3dc95e1e988ef3e6b2492225235e2d
vishnupriy-a <116629496+vishnupriy-a@users.noreply.github.com>
2022-12-05 04:28:28 -0500

    Add files via upload

---
+++ circularqueue.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<stdlib.h>
+void enqueue();
+void dequeue();
+void display();
+struct node
+{
+int data;
+struct node *next;
+};
+struct node *front=NULL,*rear=NULL;
+void main()
+{
+int ch;
+printf("\n*********MENU********\n");
+printf("\n1.insertion\n2.deletion\n3.display\n4.exit\n");
+do
+{
+printf("\nenter your choice:\n");
+scanf("%d",&ch);
+switch(ch)
+	{
+		case 1:enqueue();
+			break;
+		case 2:dequeue();
+			break;
+		case 3:display();
+			break;
+		case 4:exit(0);
+			break;
+		default:printf("\nenter a valid choice:\n");
+	}
+}while(ch!=4);
+}
+void enqueue()
+	{
+		struct node *newnode;
+		newnode=(struct node*)malloc(sizeof(struct node));
+		printf("enter the data");
+		scanf("%d",&newnode->data);
+		if((front==NULL)&&(rear==NULL))
+			{
+				front=rear=newnode;
+				rear->next=front;
+			}
+		else
+			{
+				rear->next=newnode;
+				newnode->next=front;
+				rear=newnode;
+			}
+	}
+void dequeue()
+	{
+		struct node *temp;
+		if(front==NULL)
+			{
+				printf("underflow occurs");
+			}
+		else
+			{
+				temp=front;
+				front=front->next;
+				rear->next=front;
+				free(temp);
+			}
+	}
+void display()
+	{
+		struct node *temp;
+		if(front==NULL)
+			{
+				printf("queue is empty");
+			}
+		else	
+			{
+				temp=front;
+				while(temp!=rear)
+					{
+						printf("%d-->",temp->data);
+						temp=temp->next;
+					
+
+					}
+					printf("%d-->",temp->data);
+			}
+		
+	}
+				
+
